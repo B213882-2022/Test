eddd25e736c082f8e37eb3a3bc222c5c6931668c
Riley <raters0@163.com>
2023-06-23 02:23:05 +0000

    2023/6/23 dim_reduce

--- Rscripts.R
+++ Rscripts.R
@@ -13,6 +13,11 @@ library(AnnotationDbi)
 library(Seurat)
 library(scry)
 library(scDblFinder)
+library(ggVennDiagram)
+library(ggplot2)
+library(ggpmisc)
+library(ggrepel)
+library(ggpubr)
 
 # set working directory
 setwd('/home/s2321661/Test')
@@ -124,12 +129,12 @@ sce$high_ERCC_percent <- cell_filter$high_subsets_ERCC_percent
 sce_origin <- sce  # backup
 sce
 
-# Dot plots that summarize cell QC
+# Scatter plots that summarize cell QC
 thres_total_counts <- attributes(cell_filter$low_lib_size)$thresholds[1]/1e6
 thres_detected_genes <- attributes(cell_filter$low_n_features)$thresholds[1]
 thres_mito_percent <- attributes(cell_filter$high_subsets_Mt_percent)$thresholds[2]
 thres_ERCC_percent <- attributes(cell_filter$high_subsets_ERCC_percent)$thresholds[2]
-QC_dot <- gridExtra::grid.arrange(
+QC_scatter <- gridExtra::grid.arrange(
   plotColData(sce, x="Cell.Type", y="total_counts", colour_by="discard") +
     ggtitle("Library sizes") + ylab('Total Reads Count') + xlab('Cell Type') + 
     geom_hline(yintercept = thres_total_counts,
@@ -203,7 +208,7 @@ QC_dot <- gridExtra::grid.arrange(
   ncol=2,
   nrow=2
 )
-ggsave('figures/QC_summary.pdf',QC_dot, device='pdf', width = 15, height = 15)
+ggsave('figures/QC_summary.pdf',QC_scatter, device='pdf', width = 15, height = 15)
 
 # Table summary of QC
 total.cells <- table(sce$Cell.Type)
@@ -306,9 +311,6 @@ rm(keep.cells)
 rm(keep.prop)
 
 # Venn Diagram the summarizes the failures in QC
-library(ggVennDiagram)
-library(ggplot2)
-library(ggpmisc)
 venn <- as.data.frame(cell_filter)
 QC_venn <- ggVennDiagram(apply(venn[1:4], 2, function(x) which(x == TRUE)),
                               label_alpha=0, 
@@ -322,8 +324,9 @@ QC_venn <- ggVennDiagram(apply(venn[1:4], 2, function(x) which(x == TRUE)),
   theme(plot.title = element_text(hjust = 0.5)) +
   scale_x_continuous(expand = expansion(mult = .2)) +
   scale_color_manual(values = c("deepskyblue2","darkolivegreen","darkorange","darkorchid")) +
-  annotate('table',label = QC_table[,c(1,2,7,8)],x = 0, y = 0,vjust = 0.1, hjust = -0.5) +
+  annotate('table',label = QC_table[,c(1,2,7,8)],x = 0, y = 0,vjust = 0.1, hjust = -0.4) +
   theme(legend.position = c(0.95, 0.5))
+QC_venn
 ggsave('figures/QC_venn.jpg',QC_venn, device='jpg', width = 8, height = 8)
 rm(venn)
 
@@ -362,8 +365,54 @@ QC_hist <- gridExtra::grid.arrange(
 )
 ggsave('figures/QC_hist.jpg',QC_hist, device='jpg', width = 10, height = 8)
 
-# drop cells
+# drop cells 
+# cell QC fail
 sce <- sce[, !cell_filter$discard]
+
+# remove cells with no expression of Oct4 and Sox2 (cell QC 2)
+# check how many cells are not expressing Oct4 in each cell type
+Oct4_fail <- c(
+  sum(assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),colData(sce)$Cell.Type == '2i'] == 0),
+  sum(assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),colData(sce)$Cell.Type == 'a2i'] == 0),
+  sum(assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),colData(sce)$Cell.Type == 'serum'] == 0)
+)
+Oct4_fail
+# check how many cells are not expressing Sox2 in each cell type
+Sox2_fail <- c(
+  sum(assays(sce)$counts[grepl('^Sox2$',rownames(sce)),colData(sce)$Cell.Type == '2i'] == 0),
+  sum(assays(sce)$counts[grepl('^Sox2$',rownames(sce)),colData(sce)$Cell.Type == 'a2i'] == 0),
+  sum(assays(sce)$counts[grepl('^Sox2$',rownames(sce)),colData(sce)$Cell.Type == 'serum'] == 0)
+)
+Sox2_fail
+# summary
+Cell_Types <- c('2i','a2i','serum')
+Keep <- c(
+  sum(assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),colData(sce)$Cell.Type == '2i'] != 0 &
+        assays(sce)$counts[grepl('^Sox2$',rownames(sce)),colData(sce)$Cell.Type == '2i'] != 0),
+  sum(assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),colData(sce)$Cell.Type == 'a2i'] != 0 &
+        assays(sce)$counts[grepl('^Sox2$',rownames(sce)),colData(sce)$Cell.Type == 'a2i'] != 0),
+  sum(assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),colData(sce)$Cell.Type == 'serum'] != 0 &
+        assays(sce)$counts[grepl('^Sox2$',rownames(sce)),colData(sce)$Cell.Type == 'serum'] != 0)
+)
+Keep
+Total <- c(
+  sum(colData(sce)$Cell.Type == '2i'),
+  sum(colData(sce)$Cell.Type == 'a2i'),
+  sum(colData(sce)$Cell.Type == 'serum')
+)
+QC_table_Oct4_Sox2 <- data.frame(Cell_Types, Total, Oct4_fail, Sox2_fail, Keep)
+QC_table_Oct4_Sox2
+write.csv(QC_table_Oct4_Sox2 ,'figures/QC_table_Oct4_Sox2.csv', row.names = FALSE)
+rm(Oct4_fail)
+rm(Sox2_fail)
+rm(Cell_Types)
+rm(Keep)
+rm(Total)
+
+# drop cells
+sce <- sce[,assays(sce)$counts[grepl('^Pou5f1$',rownames(sce)),] != 0]
+sce <- sce[,assays(sce)$counts[grepl('^Sox2$',rownames(sce)),] != 0]
+sce
 
 # gene filtering
 # remove spikes
@@ -377,18 +426,20 @@ detected_cell_prop.hist <- ggplot(as.data.frame(QC_stats.gene), aes(x=detected))
   xlab('Detected %') +
   ylab('Counts') + 
   ggtitle('Genes with expression >0')
+detected_cell_prop.hist
 ggsave('figures/detected_cell_prop.jpg',detected_cell_prop.hist, 
        device='jpg', width = 8, height = 6)
 rowData(sce)$mean <- QC_stats.gene$mean
 rowData(sce)$detected_prop <- QC_stats.gene$detected
 sum(rowSums(assays(sce)$counts > 0) >= 3)  # check how many genes are left 
+# remove genes
 sce <- sce[rowSums(assays(sce)$counts > 0) >= 3,]
 sce
 
 # Normalisation
 sce <- computeSumFactors(sce, cluster = quickCluster(sce))
 sce <- logNormCounts(sce)
-sce
+sce  # assays(sce) has 'logcounts'
 
 # check oct4 variation
 # raw counts
@@ -405,6 +456,7 @@ oct4_variation_logcounts
 ggsave('figures/oct4_variation_logcounts.jpg',oct4_variation_logcounts, device='jpg', width = 8, height = 10)
 
 # correlation (scatter plot)
+# positive control
 targets <- c('Nanog', 'Sox2','Klf4','Zfp42','Utf1','Esrrb')
 #rownames(sce)[grepl('Esrrb',rownames(sce))]
 # raw counts
@@ -419,68 +471,205 @@ ggsave('figures/oct4_corr_scatter_rawcounts.jpg',oct4_corr_scatter_rawcounts, de
 oct4_corr_scatter_logcounts <- plotExpression(sce,targets,x='Pou5f1',exprs_values = "logcounts",color_by = 'Cell.Type') + 
   xlab('Oct4 expression level') + 
   ylab('Expression level (log counts after normalisation)') +
-  ggtitle('Expression level of Oct4 and some of its target genes') +
+  ggtitle('Expression level of Oct4 and some of its target genes (after normalisation)') +
   guides(color=guide_legend("Cell Type"))
 oct4_corr_scatter_logcounts
 ggsave('figures/oct4_corr_scatter_logcounts.jpg',oct4_corr_scatter_logcounts, device='jpg', width = 8, height = 10)
 rm(targets)
+# negative control
+hk_genes <- c('Actb','Gapdh','Pgk1','Ppia','Rpl38','Rpl13a')
+oct4_corr_scatter_rawcounts.hk <- plotExpression(sce,hk_genes,x='Pou5f1',exprs_values = "counts",color_by = 'Cell.Type') + 
+  xlab('Oct4 expression level') + 
+  ylab('Expression level (raw counts)') +
+  ggtitle('Expression level of Oct4 and some housekeeping genes') +
+  guides(color=guide_legend("Cell Type"))
+oct4_corr_scatter_rawcounts.hk
+ggsave('figures/oct4_corr_scatter_rawcounts_hk.jpg',oct4_corr_scatter_rawcounts.hk, device='jpg', width = 8, height = 10)
+# log counts
+oct4_corr_scatter_logcounts.hk <- plotExpression(sce,hk_genes,x='Pou5f1',exprs_values = "logcounts",color_by = 'Cell.Type') + 
+  xlab('Oct4 expression level') + 
+  ylab('Expression level (log counts after normalisation)') +
+  ggtitle('Expression level of Oct4 and some housekeeping genes (after normalisation)') +
+  guides(color=guide_legend("Cell Type"))
+oct4_corr_scatter_logcounts.hk
+ggsave('figures/oct4_corr_scatter_logcounts_hk.jpg',oct4_corr_scatter_logcounts.hk, device='jpg', width = 8, height = 10)
+rm(hk_genes)
 
 # fearture selection
-# scran HVG top 1000
-# model gene variance by trend
+assays(sce)$norm_counts <- 2^(logcounts(sce))-1
+# scran HVG (model gene variance by trend)
+#some how modelGeneVarWithSpikes(sce,'spikes') fails...
 var.out <- modelGeneVar(sce)
-plot(var.out$mean, var.out$total, pch=16, cex=0.6, xlab='mean log-expression',
-     ylab="Total Variance of log-expression")
-o <- order(var.out$mean)
-lines(var.out$mean[o], var.out$tech[o], col="dodgerblue", lwd=2)
-features_hvg <- getTopHVGs(var.out,n=4000)
-top50_hvg <- plotExpression(sce,features_hvg)
-top50_hvg
-ggsave('figures/top50_hvg.jpg',top50_hvg, device='jpg', width = 10, height = 6)
-rm(o)
+features_hvg <- getTopHVGs(var.out,n=50)
+deconvolution_HVG <- ggplot(as.data.frame(var.out), aes(x=mean, y=total)) + geom_point() +
+  geom_point(data=as.data.frame(var.out[features_hvg,c('mean','total')]), color='red') +
+  geom_line(aes(x=mean,y=tech), color='skyblue') +
+  geom_text_repel(label=features_hvg, 
+            data=as.data.frame(var.out[features_hvg,c('mean','total')]),
+            color='coral1', size=3,box.padding = unit(0.2, "lines"),
+            point.padding = unit(0.25, "lines"), max.overlaps = 20) +
+  ggtitle('Technicle variation (blue line) and top 50 HVG') +
+  xlab('Mean log-expression') +
+  ylab('Total Variance of log-expression')
+deconvolution_HVG
+ggsave('figures/deconvolution_HVG.jpg', deconvolution_HVG, device='jpg', width = 10, height = 6)
+# log counts of HVG
+features_hvg <- getTopHVGs(var.out,n=100)
+top100_hvg.log <- plotExpression(sce,features_hvg,exprs_values = "logcounts") + ylim(0,20)
+top100_hvg.log
+ggsave('figures/top100_hvg_log.jpg',top100_hvg.log, device='jpg', width = 20, height = 6)
+# normalised raw counts of HVG
+top100_hvg.raw <- plotExpression(sce,features_hvg,exprs_values = "norm_counts") + ylim(0,2e+5)
+top100_hvg.raw
+ggsave('figures/top100_hvg_raw.jpg',top100_hvg.raw, device='jpg', width = 20, height = 6)
 
 # Deviance (select genes that are both highly expressed and highly variable)
 dev <- rowData(devianceFeatureSelection(sce,assay = "counts", fam='binomial'))$binomial_deviance
+# log counts of deviance
+features_dev <- names(dev[order(dev,decreasing=TRUE)])[1:100]
+top100_dev.log <- plotExpression(sce,features_dev,exprs_values = "logcounts")+ ylim(0,20)
+top100_dev.log
+ggsave('figures/top100_dev_log.jpg',top100_dev.log, device='jpg', width = 20, height = 6)
+# normalised raw counts of deviance
+top100_dev.raw <- plotExpression(sce,features_dev,exprs_values = "norm_counts") + ylim(0,2e+5)
+top100_dev.raw
+ggsave('figures/top100_dev_raw.jpg',top100_dev.raw, device='jpg', width = 20, height = 6)
+
+# check intersection between features selected by HVG and DEV
+features_hvg <- getTopHVGs(var.out,n=2000)
 features_dev <- names(dev[order(dev,decreasing=TRUE)])[1:2000]
-top50_dev <- plotExpression(sce,features_dev)
-top50_dev
-ggsave('figures/top50_dev.jpg',top50_dev, device='jpg', width = 10, height = 6)
+features_venn <- ggVennDiagram(data.frame(features_dev=features_dev,features_hvg=features_hvg)) +
+  theme(legend.position = "none") +
+  ggtitle('intersection of top 2000 features selected by Dev VS. HVG') +
+  theme(plot.title = element_text(hjust = 0.5))
+features_venn
+ggsave("figures/features_venn.jpg",features_venn,device='jpg', width = 6, height = 6)
+# mean-variance plot
+meanvar_plotdata <- function(sce,G=2000){
+  #this function is copied from: https://github.com/willtownes/scrna2019
+  #sce=SingleCellExperiment with assays "counts" and "logcounts"
+  #logcounts = log2(1+counts/scran size factor)
+  #G=number of genes to be "highly informative"
+  #assumes rowData(sce) contains columns "dev","hvg" with ranks
+  #rank=1 means gene is most informative according to the criterion
+  #returns a data frame to use for making a mean/variance plot
+  gm<-as.data.frame(rowData(sce))
+  rk<-gm[,c("dev","hvg")]
+  rk<-rk[rownames(sce),]
+  Y<-2^(logcounts(sce))-1
+  pd<-data.frame(m=rowMeans(Y),v=apply(Y,1,var))
+  pd$vmr=pd$v/pd$m
+  pd<-cbind(pd,rk)
+  pd<-subset(pd,vmr>0)
+  xt<-table(pd$dev<=G,pd$hvg<=G)
+  #formatting plot labels function
+  f<-function(s,n){paste0(s," (",prettyNum(n,big.mark=","),")")}
+  pd$criteria<-f("neither",xt[1,1])
+  pd$criteria[pd$dev<=G & pd$hvg<=G]<-f("both",xt[2,2])
+  pd$criteria[pd$dev<=G & pd$hvg>G]<-f("high deviance",xt[2,1])
+  pd$criteria[pd$dev>G & pd$hvg<=G]<-f("highly variable",xt[1,2])
+  #sort so that the "neither" category doesn't obscure the "highly deviant"
+  pd[order(pd$criteria,decreasing=TRUE),]
+}
+test.sce <- sce
+rowData(test.sce)$hvg <- rank(-var.out$bio)
+rowData(test.sce)$dev <- rank(-dev)
+#View(as.data.frame(rowData(test.sce)))
+df <- meanvar_plotdata(test.sce, G=2000)
+features_mean_var <- ggplot(df,aes(x=m,y=vmr,colour=criteria))+
+  geom_point(alpha=.9)+xlab("mean normalized expression (no log transformation)") +
+  ylab(latex2exp::TeX('$CV^2$ (variance/mean)')) +
+  theme(legend.position=c(0.2,.8),
+        plot.margin = margin(0.5, 0.5, 0.1, 0.1, "cm"))+
+  scale_color_manual(values=c("orange","red","blue","gray"))+
+  scale_x_log10()+scale_y_log10()+
+  ggtitle('mean-variance plot of top 2000 features selected by Dev VS. HVG')
+features_mean_var
+ggsave("figures/features_mean_var.jpg",features_mean_var,device='jpg', width = 8, height = 7)
+rm(test.sce)
+rm(meanvar_plotdata)
+rm(df)
 
 # Dimensionality reduction
 set.seed(213882)
 #set.seed(NULL)
+
+# tSNE perplexity test
+# HVG:1000-4000 + perplexity:10,15,20,25,30 (5行4列) 
+
+# different number of features
 # by hvg
-length(features_hvg)
-sce_hvg <- runPCA(sce,subset_row = features_hvg)
-sce_hvg <- runTSNE(sce_hvg,perplexity=10, subset_row = features_hvg)
-sce_hvg <- runUMAP(sce_hvg, subset_row = features_hvg)
-reducedDimNames(sce_hvg)
-dim_reduce_hvg <- gridExtra::grid.arrange(
-  plotReducedDim(sce_hvg, "PCA", colour_by = 'Cell.Type')
-  +ggtitle('PCA'),
-  plotReducedDim(sce_hvg, "TSNE", colour_by = 'Cell.Type')
-  +ggtitle('TSNE'),
-  plotReducedDim(sce_hvg, "UMAP", colour_by = 'Cell.Type')
-  +ggtitle('UMAP'),
-  ncol=1
-)
-ggsave('figures/dim_reduce_hvg(4000).jpg',dim_reduce_hvg, device='jpg', width = 6, height = 12)
+reduce_dim_hvg.func <- function(sce,num=c(100,250,500,750,1000,1500,2000,3000,4000),ref){
+  # num is an array of numbers
+  p <- list()
+  for(n in num){
+    print(n)
+    features_hvg <- getTopHVGs(ref,n=n)
+    sce_hvg <- runPCA(sce,subset_row = features_hvg)
+    sce_hvg <- runTSNE(sce_hvg,perplexity=10, subset_row = features_hvg)
+    sce_hvg <- runUMAP(sce_hvg, subset_row = features_hvg)
+    p[[as.character(n)]] <- annotate_figure(ggarrange(
+      plotReducedDim(sce_hvg, "PCA", colour_by = 'Cell.Type')
+      +ggtitle('PCA'),
+      plotReducedDim(sce_hvg, "TSNE", colour_by = 'Cell.Type')
+      +ggtitle('TSNE'),
+      plotReducedDim(sce_hvg, "UMAP", colour_by = 'Cell.Type')
+      +ggtitle('UMAP'),
+      nrow=3,
+      #common.legend = TRUE, 
+      legend="none"
+    ),
+    top=text_grob(paste0('top ',n,' features'), color = 'red',face = "bold"))
+  }
+  r <- list()
+  r[['plots']] <- p
+  r[['legend']] <- get_legend(plotReducedDim(sce_hvg, "PCA", colour_by = 'Cell.Type'))
+  return(r)
+}
+results <- reduce_dim_hvg.func(sce,ref=var.out)
+dim_reduce_hvg <- ggarrange(plotlist=results[['plots']], 
+                            ncol=3, nrow=3, common.legend = TRUE, legend="right", 
+                            legend.grob = results[['legend']]) +
+                  theme(plot.margin = margin(0.1,0.5,0.1,0.1, "cm"))
+dim_reduce_hvg
+ggsave('figures/dim_reduce_hvg.jpg',dim_reduce_hvg, device='jpg', width = 15, height = 35)
+rm(results)
 # by dev
-length(features_dev)
-sce_dev <- runPCA(sce,subset_row = features_dev)
-sce_dev <- runTSNE(sce_dev,perplexity=10, subset_row = features_dev)
-sce_dev <- runUMAP(sce_dev, subset_row = features_dev)
-reducedDimNames(sce_hvg)
-dim_reduce_dev <- gridExtra::grid.arrange(
-  plotReducedDim(sce_dev, "PCA", colour_by = 'Cell.Type')
-  +ggtitle('PCA'),
-  plotReducedDim(sce_dev, "TSNE", colour_by = 'Cell.Type')
-  +ggtitle('TSNE'),
-  plotReducedDim(sce_dev, "UMAP", colour_by = 'Cell.Type')
-  +ggtitle('UMAP'),
-  ncol=1
-)
-ggsave('figures/dim_reduce_dev(2000).jpg',dim_reduce_dev, device='jpg', width = 6, height = 12)
+reduce_dim_dev.func <- function(sce,num=c(100,250,500,750,1000,1500,2000,3000,4000),ref){
+  # num is an array of numbers
+  p <- list()
+  for(n in num){
+    print(n)
+    features_dev <- names(ref[order(ref,decreasing=TRUE)])[1:n]
+    sce_dev <- runPCA(sce,subset_row = features_dev)
+    sce_dev <- runTSNE(sce_dev,perplexity=10, subset_row = features_dev)
+    sce_dev <- runUMAP(sce_dev, subset_row = features_dev)
+    p[[as.character(n)]] <- annotate_figure(ggarrange(
+      plotReducedDim(sce_dev, "PCA", colour_by = 'Cell.Type')
+      +ggtitle('PCA'),
+      plotReducedDim(sce_dev, "TSNE", colour_by = 'Cell.Type')
+      +ggtitle('TSNE'),
+      plotReducedDim(sce_dev, "UMAP", colour_by = 'Cell.Type')
+      +ggtitle('UMAP'),
+      nrow=3,
+      #common.legend = TRUE, 
+      legend="none"
+    ),
+    top=text_grob(paste0('top ',n,' features'), color = 'red',face = "bold"))
+  }
+  r <- list()
+  r[['plots']] <- p
+  r[['legend']] <- get_legend(plotReducedDim(sce_dev, "PCA", colour_by = 'Cell.Type'))
+  return(r)
+}
+results <- reduce_dim_dev.func(sce,ref=dev)
+dim_reduce_dev <- ggarrange(plotlist=results[['plots']], 
+                            ncol=3, nrow=3, common.legend = TRUE, legend="right", 
+                            legend.grob = results[['legend']]) +
+                  theme(plot.margin = margin(0.1,0.5,0.1,0.1, "cm"))
+dim_reduce_dev
+ggsave('figures/dim_reduce_dev.jpg',dim_reduce_dev, device='jpg', width = 15, height = 35)
+rm(results)
 
 # Doublet test
 dbl.dens <- computeDoubletDensity(sce_dev, subset.row=features_dev, 
